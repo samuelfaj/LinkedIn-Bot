8e0d7057cc7a326744bd2719b3a1c8cb3529b2d7
Samuel Faj <samuelfaj@icloud.com>
2018-02-17 18:19:55 -0200

    Sending the first version

--- LinkedInBot.php
+++ LinkedInBot.php
@@ -3,29 +3,35 @@
      ___________
     / _   _     \
    | | | ' '__   |
-   | | | |  _  \ | LINKEDIN BOT v.1.0
-   | | | | | | | |    by Samuel Faj
+   | | | |  _  \ |     LINKEDIN BOT v.1.0
+   | | | | | | | |        by Samuel Faj
    | |_| |_| |_| |
     \___________/
 
 */
 
 /* LinkedIn User Configurations */
-$username = 'user@email.com';
-$password = 'password1234';
+$username = 'user@email.com.br';
+$password = 'mysecretpass';
 
 /* Bot Configurations */
-$maxCycles = 0;                          # 0 = unlimited.
-$sleepBetweenCycles = rand(5,30);        # How much to wait, in seconds, between a cycle and another.
+$maxCycles = 0;                                     # 0 = unlimited.
+$sleepBetweenCycles = rand(5 * 60, 10 * 60);        # How much to wait, in seconds, between a cycle and another.
 
 /* Cycle Configurations */
-$likesOnFeed = 10;                       # 0 = unlimited.
-$maxNewConnections = 50;                 # 0 = unlimited.
+// For each cycle the script will do:
+$autolike = true;                        # Automatically likes posts on feed.
+$likesOnFeed = 30;                       # 0 = unlimited
+
+$autoInvite = true;                      # Automatically invite people to your network.
+$maxNewConnections = 60;                 # 0 = unlimited
+
+$autoAdd = true;                         # Automatically accept invites from users.
 
 /* Technicals Configurations */
 $host = 'http://localhost:4444/wd/hub';  # Selenium default host
 
 require_once('bot/robot.php');
 
-$robot = new Facebook\WebDriver\LinkedInBot($username,$password,$maxCycles,$sleepBetweenCycles,$likesOnFeed,$maxNewConnections,$host);
+$robot = new Facebook\WebDriver\LinkedInBot($username,$password,$maxCycles,$sleepBetweenCycles,$likesOnFeed,$maxNewConnections,$host,$autolike,$autoInvite,$autoAdd);
 $robot->start();
--- bot/robot.php
+++ bot/robot.php
@@ -23,8 +23,9 @@ class LinkedInBot{
     public $cycle   = 0;
     public $likes   = 0;
     public $invites = 0;
+    public $accepts = 0;
 
-    public function __construct($username,$password,$maxCycles,$sleepBetweenCycles,$likesOnFeed,$maxNewConnections,$host){
+    public function __construct($username,$password,$maxCycles,$sleepBetweenCycles,$likesOnFeed,$maxNewConnections,$host,$autolike,$autoInvite,$autoAdd){
         $this->host = $host;
         $this->username = $username; 
         $this->password = $password;
@@ -32,6 +33,10 @@ class LinkedInBot{
         $this->likesOnFeed = $likesOnFeed;
         $this->maxNewConnections  = $maxNewConnections;
         $this->sleepBetweenCycles = $sleepBetweenCycles;
+
+        $this->autoAdd  = $autoAdd;
+        $this->autolike = $autolike;
+        $this->autoInvite = $autoInvite;
 
         $this->capabilities = DesiredCapabilities::chrome();
         $this->driver       = RemoteWebDriver::create($this->host, $this->capabilities, 5000);
@@ -41,16 +46,31 @@ class LinkedInBot{
         $this->getIntoLinkedIn();
         $this->startCycle();
     }
+
+    public function check(){
+        $url = $this->driver->getCurrentURL();
+
+        if(strpos($url,'login') !== false){
+            $this->driver->findElement(WebDriverBy::id('session_key-login'))->sendKeys($this->username);
+            $this->driver->findElement(WebDriverBy::id('session_password-login'))->sendKeys($this->password);
+            $this->driver->findElement(WebDriverBy::id('btn-primary'))->click();
+        }
+    }
     
     public function getIntoLinkedIn(){
         echo 'Logging into LinkedIn...'.PHP_EOL;
-         $this->driver->get('https://www.linkedin.com/'); // realizando uma requisição HTTP get na $url
-        try{
-             $this->driver->findElement(WebDriverBy::id('login-email'))->sendKeys($this->username);
-             $this->driver->findElement(WebDriverBy::id('login-password'))->sendKeys($this->password);
-             $this->driver->findElement(WebDriverBy::id('login-submit'))->click();
-        } catch (Exception $e) {
-            echo '    ERROR - May not be in login page.'.PHP_EOL;
+        $this->driver->get('https://www.linkedin.com/'); // realizando uma requisição HTTP get na $url
+        $this->check();
+
+        $elements =  $this->driver->findElements(WebDriverBy::cssSelector('nav#extended-nav'));
+        if(count($elements) == 0){
+            try{
+                $this->driver->findElement(WebDriverBy::id('login-email'))->sendKeys($this->username);
+                $this->driver->findElement(WebDriverBy::id('login-password'))->sendKeys($this->password);
+                $this->driver->findElement(WebDriverBy::id('login-submit'))->click();
+            } catch (Exception $e) {
+                echo '    ERROR - May not be in login page.'.PHP_EOL;
+            }
         }
     }
 
@@ -63,6 +83,7 @@ class LinkedInBot{
             $this->autoLike();
 
             $this->invites = 0;
+            $this->accepts = 0;
             $this->autoInvite();
 
 
@@ -82,31 +103,46 @@ class LinkedInBot{
     public function autoLike(){
         $hasfeed = true;
 
+        if(!$this->autolike) return false;
+
         echo '    Getting into Feed...'.PHP_EOL;
         while($hasfeed && ($this->likesOnFeed == 0 || $this->likes <= $this->likesOnFeed)){
-            $this->driver->get('https://www.linkedin.com/feed/');
 
-            do{
-                try{
-                    $elements =  $this->driver->findElements(WebDriverBy::cssSelector('article div button.like-button:not(.active):first-of-type'));
-                    foreach ($elements as $i=>$element){
-                        if($this->likes > $this->likesOnFeed) continue;
+            if($this->likes > 0 || $this->driver->getCurrentURL() !== 'https://www.linkedin.com/feed/') {
+                $this->driver->get('https://www.linkedin.com/feed/');
+            }
 
-                        if($element->isEnabled() && $element->isDisplayed()){
-                            $element->getLocationOnScreenOnceScrolledIntoView();
+            $this->check();
 
-                            $id = $element->getAttribute('id');
-                            $this->driver->executeScript('$("#'.$id.'").click()',array());
+            try{
+                $this->driver->wait(10, 500)->until(
+                    WebDriverExpectedCondition::presenceOfElementLocated(
+                        WebDriverBy::cssSelector('article div button.like-button:not(.active):first-of-type')
+                    )
+                );
 
-                            $this->likes++;
-                            echo '        ' . $this->likes . '/' . $this->likesOnFeed . ' => Linking post... ' . PHP_EOL;
+                do{
+                    try{
+                        $elements =  $this->driver->findElements(WebDriverBy::cssSelector('article div button.like-button:not(.active):first-of-type'));
+                        foreach ($elements as $i=>$element){
+                            if($this->likes > $this->likesOnFeed) continue;
 
-                            sleep(2);
+                            if($element->isEnabled() && $element->isDisplayed()){
+                                $element->getLocationOnScreenOnceScrolledIntoView();
+
+                                $id = $element->getAttribute('id');
+                                $this->driver->executeScript('$("#'.$id.'").click()',array());
+
+                                $this->likes++;
+                                echo '        ' . $this->likes . '/' . $this->likesOnFeed . ' => Linking post... ' . PHP_EOL;
+
+                                sleep(rand(5,20));
+                            }
                         }
-                    }
-                }catch (Exception $e2){ echo '        ERROR - ' . $e2 . PHP_EOL; }
+                    }catch (Exception $e2){ echo '        ERROR - ' . $e2 . PHP_EOL; }
 
-            }while(count($elements) > 0 && $this->likes <= $this->likesOnFeed);
+                }while(count($elements) > 0 && $this->likes <= $this->likesOnFeed);
+            }catch (\Exception $e){ echo '        ERROR - ' . $e . PHP_EOL;  }
 
             if(count($elements) == 0) $hasfeed = false;
         }
@@ -118,25 +154,58 @@ class LinkedInBot{
             echo '    Getting into My Network...'.PHP_EOL;
 
             $this->driver->get('https://www.linkedin.com/mynetwork/');
-            do{
-                $elements =  $this->driver->findElements(WebDriverBy::cssSelector('.mn-pymk-list__cards'));
-                foreach ($elements as $i=>$element){
-                    if($this->invites > $this->maxNewConnections) continue;
 
-                    $user = $element->findElement(WebDriverBy::cssSelector('span.mn-person-info__name'));
-                    echo '        '.$this->invites.'/'.$this->maxNewConnections.' => Connecting with '.$user->getText().'...'.PHP_EOL;
+            $this->check();
 
-                    $element->findElement(WebDriverBy::cssSelector('.button-secondary-small'))->click();
-                    sleep(rand(2,4));
+            try{
+                $this->driver->wait(10, 500)->until(
+                    WebDriverExpectedCondition::presenceOfElementLocated(WebDriverBy::cssSelector('.mn-pymk-list__card'))
+                );
 
-                    $this->invites++;
-                }
-            }while(count($elements) > 0 && $this->invites <= $this->maxNewConnections);
+                $this->autoAdd();
+
+                if(!$this->autoInvite) return false;
+
+                do{
+                    $elements =  $this->driver->findElements(WebDriverBy::cssSelector('.mn-pymk-list__card'));
+                    foreach ($elements as $i=>$element){
+                        if($this->invites > $this->maxNewConnections) continue;
+
+                        try{
+                            $user = $element->findElement(WebDriverBy::cssSelector('span.mn-person-info__name'));
+                            echo '        '.$this->invites.'/'.$this->maxNewConnections.' => Sending an invite to '.$user->getText().'...'.PHP_EOL;
+
+                            $button = $element->findElement(WebDriverBy::cssSelector('.button-secondary-small'));
+                            $button->getLocationOnScreenOnceScrolledIntoView();
+                            $button->click();
+                        }catch (\Exception $e){ echo '        ERROR - ' . $e . PHP_EOL; }
+
+                        sleep(rand(5,10));
+
+                        $this->invites++;
+                    }
+                }while(count($elements) > 0 && $this->invites <= $this->maxNewConnections);
+            }catch (\Exception $e){ echo '        ERROR - ' . $e . PHP_EOL;  }
+
 
             echo PHP_EOL;
             echo '    No more possible connections in this page. ' . PHP_EOL;
             echo '        => Reloading...' . PHP_EOL;
             echo PHP_EOL;
+        }
+    }
+    public function autoAdd(){
+        if(!$this->autoAdd) return false;
+
+        $elements =  $this->driver->findElements(WebDriverBy::cssSelector('.mn-invitation-card'));
+        foreach ($elements as $i=>$element){
+            echo '        '.$this->accepts.'/'.$this->accepts.' => Accepting user invite.'.PHP_EOL;
+
+            $id = $element->getAttribute('id');
+            $this->driver->executeScript('$("#'.$id.' button[data-control-name=\"accept\"]").click()',array());
+
+            $this->accepts++;
+            sleep(rand(5,10));
         }
     }
 }
